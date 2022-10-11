e332abdaef38b671e369319e4c38a4d5ed42fd11
JeonSeongHee <wjstjd1004@naver.com>
2022-10-12 00:36:34 +0900

    판정선 및 노트 애니메이션 생성

---
+++ Assets/Script/NoteBehavior.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehavior : MonoBehaviour
+{
+    public int noteType;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * GameManager.instance.noteSpeed);
+    }
+}
